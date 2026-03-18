1ab1e793082e2b5cb45dfc24ab02674528a3b468
nat06-dot <suphachokchosanthia@gmail.com>
2026-03-18 21:45:12 +0700

    reservation

--- src/components/CardReservations.tsx
+++ src/components/CardReservations.tsx
@@ -18,7 +18,7 @@ export default function CardRerservations({
   return (
     <div className="mt-[1] w-[35rem] h-[20rem] relative flex flex-col items-start gap-[1.5rem] text-left text-[2rem] text-[#724a15] font-playfair-display">
       <div className="self-stretch h-[19.438rem] grid [justify-content:start] [align-content:start] gap-[0rem] py-[0rem] pl-[0rem] pr-[40.75rem] box-border text-[1rem]">
-        <div className="w-full h-full absolute top-[0rem] left-[0rem] shadow-[0px_10px_15px_-3px_rgba(0,_0,_0,_0.1),_0px_4px_6px_-4px_rgba(0,_0,_0,_0.1)] rounded-2xl bg-white border-[#F8E9A1] border-solid border-[2px] box-border">
+        <div className="w-[30rem] h-full absolute top-[0rem] left-[0rem] shadow-[0px_10px_15px_-3px_rgba(0,_0,_0,_0.1),_0px_4px_6px_-4px_rgba(0,_0,_0,_0.1)] rounded-2xl bg-white border-[#F8E9A1] border-solid border-[2px] box-border">
           <HeadCard name={name} />
           <div className="absolute top-[7.063rem] left-[2.125rem] w-[40rem] h-[6rem] flex flex-col items-start gap-[0.75rem] text-[#724a15] font-inter">
             <CardTime time={time} />
--- src/components/ReservationHeader.tsx
+++ src/components/ReservationHeader.tsx
@@ -1,16 +1,16 @@
 export default function ReservationHeader({ count }: { count: number }) {
   return (
     <div className="flex flex-col items-start">
-      <h1 className="text-[3rem] font-bold text-[#8b4515] m-0 leading-none font-playfair-display mt-[6rem]">
+      <h1 className="text-[2rem] font-bold text-[#8b4515] m-0 leading-none font-playfair-display mt-[6rem]">
         My Reservations
       </h1>
-      <p className="text-[2rem]  text-[#E1AB64] m-0 font-playfair-display ">
+      <p className="text-[1.5rem]  text-[#E1AB64] m-0 font-playfair-display ">
         Manage your dining reservations
       </p>
-      <div className="mt-[3rem] self-stretch flex flex-col items-start text-[#8b4515] text-[2rem]">
+      <div className="mt-[3rem] self-stretch flex flex-col items-start text-[#8b4515] text-[1.5rem]">
         <div className="self-stretch h-[2.25rem] relative">
           <b className="absolute top-[0rem] left-[0rem] leading-[2.25rem]">
-            Upcoming ({count})
+            Upcoming ( {count} )
           </b>
         </div>
       </div>
